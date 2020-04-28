d3772dde43ef152d3136918b5fb87bf2081b3539
H@di <hadi@hadisafari.ir>
2020-04-28 20:15:47 +0430

    add log to twtr parser

--- twtr/parser.py
+++ twtr/parser.py
@@ -1,5 +1,6 @@
 import json
 import re
+from sys import stderr
 
 
 class Parser:
@@ -10,11 +11,13 @@ class Parser:
 
     def get_msgs_as_text(self):
         with open(self.addr) as f:
-            return [
+            res = [
                 self._clean(tweet["tweet"])
                 for tweet in [json.loads(raw_tweet) for raw_tweet in f]
                 if len(tweet["reply_to"]) == 1
             ]
+            print("%d tweets" % len(res), file=stderr)
+            return res
 
     def _clean(self, msg):
         msg = re.sub(r"pic\.twitter\.com\/\S*", "", msg)
